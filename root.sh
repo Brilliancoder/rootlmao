36180ee5fb92026c01d91d0b474649600a921115
Brilliancoder <140956295+Brilliancoder@users.noreply.github.com>
2024-07-26 20:04:40 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -6,4 +6,11 @@ unzip root.zip
 
 tar -xvf root.tar.xz
 
+echo cleaning some files
+rm music.mp3
+rm video.mp4
+rm root.zip
+rm root.tar.xz
+rm yt.zip
+echo done
 echo use ./dist/proot -S . /bin/bash to start root environment using proot
